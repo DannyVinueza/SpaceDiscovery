ee32e5313ecbc521d18eba24c58f903820447346
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-06-13 00:17:35 -0500

    Pagina home

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const app = express()
 
 // Configuraciones
-app.set('port', process.env.port || 4000)
+app.set('port', process.env.port || 3000)
 app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', engine({
   defaultLayout: 'main',
@@ -23,8 +23,19 @@ app.use(express.urlencoded({ extended: false }))
 //Variables globales
 
 //Rutas
-app.get("/", (req, res) => {
-  res.render("index")
+app.get("/", async (req, res) => {
+  try {
+    // Realizar una solicitud a la API de Spaceflight News
+    const response = await axios.get('https://api.spaceflightnewsapi.net/v3/reports');
+
+    // Obtener los datos de los artículos de la respuesta
+    const reports = response.data;
+    console.log(reports);
+    res.render('index', { reports });
+  } catch (error) {
+    console.error('Error al obtener reports:', error);
+    res.render('error'); // Renderizar una plantilla de error en caso de fallo
+  }
 })
 
 app.get('/noticias', async (req, res) => {
@@ -34,7 +45,7 @@ app.get('/noticias', async (req, res) => {
 
     // Obtener los datos de los artículos de la respuesta
     const articles = response.data;
-console.log(articles);
+    console.log(articles);
     res.render('news', { articles });
   } catch (error) {
     console.error('Error al obtener noticias:', error);
